769b7b26c628130963990473723a300387833347
pillow34 <rane.nupur11@gmail.com>
2020-08-16 12:31:51 +0530

    solution to question 5 and 6

--- archive/eulerfuncs.py
+++ archive/eulerfuncs.py
@@ -21,3 +21,54 @@ def isPrime(n):
         i = i + 6
 
     return True
+
+
+def findGCF(n=[1]):
+    a = []
+    for i in range(int(min(n))+1):
+        a.append(i) if max(np.array(n) % i) == 0 else 1
+    return max(a)
+
+
+def findLCM(arr):
+    n = len(arr)
+    # Find the maximum value in arr[]
+    max_num = 0
+    for i in range(n):
+        if (max_num < arr[i]):
+            max_num = arr[i]
+
+    # Initialize result
+    res = 1
+
+    # Find all factors that are present
+    # in two or more array elements.
+    x = 2  # Current factor.
+    while (x <= max_num):
+
+        # To store indexes of all array
+        # elements that are divisible by x.
+        indexes = []
+        for j in range(n):
+            if (arr[j] % x == 0):
+                indexes.append(j)
+
+        # If there are 2 or more array
+        # elements that are divisible by x.
+        if (len(indexes) >= 2):
+
+            # Reduce all array elements
+            # divisible by x.
+            for j in range(len(indexes)):
+                arr[indexes[j]] = int(arr[indexes[j]] / x)
+
+            res = res * x
+        else:
+            x += 1
+
+    # Then multiply all reduced
+    # array elements
+    for i in range(n):
+        res = res * arr[i]
+
+    return res
--- archive/q6.py
+++ archive/q6.py
@@ -1,12 +1,11 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
-import eulerfuncs as ef
-
 
 def compute():
-    y = [i for i in range(1, 21)]
-    return ef.findLCM(y)
+    y = [i for i in range(1, 101)]
+    z = [i * i for i in range(1, 101)]
+    return sum(y)**2 - sum(z)
 
 
 if __name__ == "__main__":
