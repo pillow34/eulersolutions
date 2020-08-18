a3018434482aeef9ae2812966ccaeb2bdf11a09e
pillow34 <rane.nupur11@gmail.com>
2020-08-19 02:30:41 +0530

    solution to question 15

---
+++ archive/q15.py
@@ -0,0 +1,13 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import eulerfuncs as ef
+import math
+
+
+def compute():
+    return math.comb(40, 20)
+
+
+if __name__ == "__main__":
+    print(compute())
