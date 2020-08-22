d76658d845865dc12c7e8f150e23f21395487c74
pillow34 <rane.nupur11@gmail.com>
2020-08-22 14:20:14 +0530

    changed solution to question 3

--- archive/eulerfuncs.py
+++ archive/eulerfuncs.py
@@ -1,5 +1,8 @@
 import numpy as np
 from itertools import product
+import math
+from functools import reduce
+from collections import Counter
 
 
 def isPrime(n):
@@ -72,3 +75,37 @@ def findLCM(arr):
         res = res * arr[i]
 
     return res
+
+
+MUL = int.__mul__
+
+
+def prime_factors(n):
+    'Map prime factors to their multiplicity for n'
+    d = _divs(n)
+    d = [] if d == [n] else (d[:-1] if d[-1] == d else d)
+    pf = Counter(d)
+    return dict(pf)
+
+
+def _divs(n):
+    'Memoized recursive function returning prime factors of n as a list'
+    for i in range(2, int(math.sqrt(n)+1)):
+        d, m  = divmod(n, i)
+        if not m:
+            return [i] + _divs(d)
+    return [n]
+
+
+def proper_divs(n):
+    '''Return the set of proper divisors of n.'''
+    pf = prime_factors(n)
+    pfactors, occurrences = pf.keys(), pf.values()
+    multiplicities = product(*(range(oc + 1) for oc in occurrences))
+    divs = {reduce(MUL, (pf**m for pf, m in zip(pfactors, multis)), 1)
+            for multis in multiplicities}
+    try:
+        divs.remove(n)
+    except KeyError:
+        pass
+    return divs or ({1} if n != 1 else set())
--- archive/q3.py
+++ archive/q3.py
@@ -5,14 +5,8 @@ import eulerfuncs as ef
 
 
 def compute():
-    ans = []
     y = 600851475143
-    k = 5
-    while k >= 5 and k <= int(ef.np.sqrt(y)):
-        if ef.isPrime(k) and y % k == 0:
-            ans.append(k)
-        k = k + 6
-
+    ans = ef._divs(y)
     return max(ans)
 
 
